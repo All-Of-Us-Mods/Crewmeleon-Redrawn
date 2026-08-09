fa70cef79601f2f60e7009e948b86f0c5b4e36b4
Chipseq <108542751+xChipseq@users.noreply.github.com>
2026-08-10 01:36:28 +0200

    add taunt

--- Crewmeleon-Redrawn/Networking/TauntRpc.cs
+++ Crewmeleon-Redrawn/Networking/TauntRpc.cs
@@ -15,12 +15,13 @@ public static class TauntRpc
 
     public static List<LoadableAudioResourceAsset> TauntSounds =
     [
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_1.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_2.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_3.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_4.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_5.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_6.wav"),
-        new LoadableAudioResourceAsset("Crewmeleon-Redrawn.Resources.Sounds.taunt_7.wav")
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_1.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_2.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_3.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_4.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_5.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_6.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_7.wav"),
+        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_8.wav")
     ];
 }
