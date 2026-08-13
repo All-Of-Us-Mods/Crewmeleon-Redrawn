7288791f718b7f930de55863b5ad2537e0e009ca
PalmForest <realpalmforest@gmail.com>
2026-08-13 19:09:52 +1000

    Clean up CrewmeleonAssets and resource paths

--- Crewmeleon-Redrawn/CrewmeleonAssets.cs
+++ Crewmeleon-Redrawn/CrewmeleonAssets.cs
@@ -6,42 +6,47 @@ namespace CrewmeleonRedrawn;
 public class CrewmeleonAssets
 {
     private const string SpritesPath = "Crewmeleon-Redrawn.Resources.Sprites";
+    private const string SoundsPath = "Crewmeleon-Redrawn.Resources.Sounds";
 
     private const string ButtonSpritesPath = SpritesPath + ".Buttons";
-    public static readonly LoadableAsset<Sprite> LockButton = new LoadableResourceAsset(ButtonSpritesPath + ".Lock.png");
-    public static readonly LoadableAsset<Sprite> UnlockButton = new LoadableResourceAsset(ButtonSpritesPath + ".Unlock.png");
-    public static readonly LoadableAsset<Sprite> PaintButton = new LoadableResourceAsset(ButtonSpritesPath + ".Paint.png");
+    public static readonly LoadableAsset<Sprite> LockButton     = new LoadableResourceAsset(ButtonSpritesPath + ".Lock.png");
+    public static readonly LoadableAsset<Sprite> UnlockButton   = new LoadableResourceAsset(ButtonSpritesPath + ".Unlock.png");
+    public static readonly LoadableAsset<Sprite> PaintButton    = new LoadableResourceAsset(ButtonSpritesPath + ".Paint.png");
 
-    public static readonly LoadableAsset<Sprite> PlayerSprite = new LoadableResourceAsset(SpritesPath + ".PlayerSprite.png");
-    public static readonly LoadableAsset<Sprite> PlayerSpriteOutline = new LoadableResourceAsset(SpritesPath + ".PlayerOutline.png");
-    public static readonly LoadableAsset<Sprite> ZoomFrame = new LoadableResourceAsset(SpritesPath + ".ZoomFrame.png");
-    public static readonly LoadableAsset<Sprite> ColorWheel = new LoadableResourceAsset(SpritesPath + ".ColorWheel.png");
-    public static readonly LoadableAsset<Sprite> BrushCursor = new LoadableResourceAsset(SpritesPath + ".BrushCursor.png");
-    public static readonly LoadableAsset<Sprite> ColorSwatch = new LoadableResourceAsset(SpritesPath + ".ColorSwatch.png");
-    
-    public static readonly LoadableAsset<Sprite> Shotgun = new LoadableResourceAsset(SpritesPath + ".Shotgun.png");
-    public static readonly LoadableAsset<Sprite> SplatterShotgun = new LoadableResourceAsset(SpritesPath + ".SplatterShotgun.png");
-    public static readonly LoadableAsset<Sprite> MuzzleFlash = new LoadableResourceAsset(SpritesPath + ".MuzzleFlash.png");
-    public static readonly LoadableAsset<Sprite> Hands = new LoadableResourceAsset(SpritesPath + ".Hands.png");
-    public static readonly LoadableAsset<Sprite> TargetSprite = new LoadableResourceAsset(SpritesPath + ".Target.png");
-    public static readonly LoadableAudioResourceAsset ShotgunFireSound = new("Crewmeleon-Redrawn.Resources.Sounds.Shotgun.wav");
+    private const string PaintingUISpritesPath = SpritesPath + ".UI.Painting";
+    public static readonly LoadableAsset<Sprite> PlayerSprite           = new LoadableResourceAsset(PaintingUISpritesPath + ".PlayerSprite.png");
+    public static readonly LoadableAsset<Sprite> PlayerSpriteOutline    = new LoadableResourceAsset(PaintingUISpritesPath + ".PlayerOutline.png");
+    public static readonly LoadableAsset<Sprite> ZoomFrame              = new LoadableResourceAsset(PaintingUISpritesPath + ".ZoomFrame.png");
+    public static readonly LoadableAsset<Sprite> ColorWheel             = new LoadableResourceAsset(PaintingUISpritesPath + ".ColorWheel.png");
+    public static readonly LoadableAsset<Sprite> BrushCursor            = new LoadableResourceAsset(PaintingUISpritesPath + ".BrushCursor.png");
+    public static readonly LoadableAsset<Sprite> ColorSwatch            = new LoadableResourceAsset(PaintingUISpritesPath + ".ColorSwatch.png");
+
+    private const string ShotgunSpritesPath = SpritesPath + ".Shotgun";
+    public static readonly LoadableAsset<Sprite> Shotgun            = new LoadableResourceAsset(ShotgunSpritesPath + ".Shotgun.png");
+    public static readonly LoadableAsset<Sprite> SplatterShotgun    = new LoadableResourceAsset(ShotgunSpritesPath + ".SplatterShotgun.png");
+    public static readonly LoadableAsset<Sprite> MuzzleFlash        = new LoadableResourceAsset(ShotgunSpritesPath + ".MuzzleFlash.png");
+    public static readonly LoadableAsset<Sprite> Hands              = new LoadableResourceAsset(ShotgunSpritesPath + ".Hands.png");
+    public static readonly LoadableAsset<Sprite> TargetSprite       = new LoadableResourceAsset(ShotgunSpritesPath + ".Target.png");
+    public static readonly LoadableAudioResourceAsset ShotgunFireSound = new(SoundsPath + ".Shotgun.wav");
 
     private const string SplatterSpritesPath = SpritesPath + ".Splatters";
-    public static List<LoadableResourceAsset> SplatterSprites =
+    public static readonly List<LoadableResourceAsset> SplatterSprites =
     [
-        new(SplatterSpritesPath + ".Splatter1.png"), new(SplatterSpritesPath + ".Splatter2.png"),
-        new(SplatterSpritesPath + ".Splatter3.png"), new(SplatterSpritesPath + ".Splatter4.png"),
+        new(SplatterSpritesPath + ".Splatter1.png"),
+        new(SplatterSpritesPath + ".Splatter2.png"),
+        new(SplatterSpritesPath + ".Splatter3.png"),
+        new(SplatterSpritesPath + ".Splatter4.png"),
     ];
 
     public static readonly List<LoadableAudioResourceAsset> TauntSounds =
     [
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_1.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_2.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_3.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_4.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_5.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_6.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_7.wav"),
-        new("Crewmeleon-Redrawn.Resources.Sounds.taunt_8.wav")
+        new(SoundsPath + ".taunt_1.wav"),
+        new(SoundsPath + ".taunt_2.wav"),
+        new(SoundsPath + ".taunt_3.wav"),
+        new(SoundsPath + ".taunt_4.wav"),
+        new(SoundsPath + ".taunt_5.wav"),
+        new(SoundsPath + ".taunt_6.wav"),
+        new(SoundsPath + ".taunt_7.wav"),
+        new(SoundsPath + ".taunt_8.wav")
     ];
 }
