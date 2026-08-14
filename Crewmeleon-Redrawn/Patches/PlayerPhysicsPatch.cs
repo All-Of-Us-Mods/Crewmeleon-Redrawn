6eb8039d0e5964937e9041ba8b92e042270b5de6
ang-xd <angxlwtff@proton.me>
2026-08-14 18:56:11 -0400

    Update gamemode icon and game options

--- Crewmeleon-Redrawn/Patches/PlayerPhysicsPatch.cs
+++ Crewmeleon-Redrawn/Patches/PlayerPhysicsPatch.cs
@@ -1,3 +1,4 @@
+using CrewmeleonRedrawn.GameMode;
 using CrewmeleonRedrawn.States;
 using HarmonyLib;
 
@@ -7,12 +8,19 @@ namespace CrewmeleonRedrawn.Patches;
  * state doesnt care about buttons, shouldnt break anything, if it does we can get another modifier for buttons
  * or add them to the modifier
  */
-[HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.TrueSpeed), MethodType.Getter)]
+[HarmonyPatch(typeof(PlayerPhysics))]
 public static class PlayerPhysicsPatch
 {
-    public static void Postfix(PlayerPhysics __instance, ref float __result)
+    [HarmonyPatch(nameof(PlayerPhysics.TrueSpeed), MethodType.Getter)]     [HarmonyPrefix]
+    public static bool TrueSpeedPatch(PlayerPhysics __instance, ref float __result)
     {
         if (!__instance.myPlayer.CanMove())
-            __result = 0;
+        {
+            __result = 0f;
+            return false;
+        }
+        if (!ChameleonGameModeManager.Instance) return true;
+        __result = __instance.Speed * ChameleonGameModeManager.Instance!.GetPlayerSpeed(__instance.myPlayer);
+        return false;
     }
 }
