2ea7e77d867da682831b217f33ea97c12e960043
XaineDev <125938059+XaineDev@users.noreply.github.com>
2026-08-15 13:25:55 +0100

    fix: inlining on mobile

--- Crewmeleon-Redrawn/Patches/PlayerPhysicsPatch.cs
+++ Crewmeleon-Redrawn/Patches/PlayerPhysicsPatch.cs
@@ -7,20 +7,33 @@ namespace CrewmeleonRedrawn.Patches;
 /*
  * state doesnt care about buttons, shouldnt break anything, if it does we can get another modifier for buttons
  * or add them to the modifier
+ *
+ * TrueSpeed is inlined on mobile and SpeedMod is inlined on pc
  */
 [HarmonyPatch(typeof(PlayerPhysics))]
 public static class PlayerPhysicsPatch
 {
+    [HarmonyPatch(nameof(PlayerPhysics.SpeedMod), MethodType.Getter)]      [HarmonyPrefix]
+    public static bool SpeedModPatch(PlayerPhysics __instance, ref float __result) =>
+        !TryGetSpeedMod(__instance, out __result);
+    
     [HarmonyPatch(nameof(PlayerPhysics.TrueSpeed), MethodType.Getter)]     [HarmonyPrefix]
     public static bool TrueSpeedPatch(PlayerPhysics __instance, ref float __result)
     {
-        if (!__instance.myPlayer.CanMove())
-        {
-            __result = 0f;
-            return false;
-        }
-        if (!ChameleonGameModeManager.Instance) return true;
-        __result = __instance.Speed * ChameleonGameModeManager.Instance!.GetPlayerSpeed(__instance.myPlayer);
+        if (!TryGetSpeedMod(__instance, out var speedMod)) return true;
+        __result = __instance.Speed * speedMod;
         return false;
+    }
+    
+    private static bool TryGetSpeedMod(PlayerPhysics physics, out float speedMod)
+    {
+        speedMod = 0f;
+        var player = physics.myPlayer;
+        if (!player.CanMove()) return true;
+        if (!ChameleonGameModeManager.Instance) return false;
+        
+        speedMod = ChameleonGameModeManager.Instance!.GetPlayerSpeed(player);
+        if (player.Data && player.Data.IsDead) speedMod *= physics.GhostSpeed / physics.Speed;
+        return true;
     }
 }
