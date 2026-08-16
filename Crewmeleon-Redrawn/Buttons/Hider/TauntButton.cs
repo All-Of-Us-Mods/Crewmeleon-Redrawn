13b021c70405129fe5d87629dd5ba08e4c4ef35b
PalmForest <realpalmforest@gmail.com>
2026-08-16 12:10:08 +1000

    Enable taunts while spectating

--- Crewmeleon-Redrawn/Buttons/Hider/TauntButton.cs
+++ Crewmeleon-Redrawn/Buttons/Hider/TauntButton.cs
@@ -30,7 +30,6 @@ public class TauntButton : CustomActionButton
     {
         return role is HiderRole
                && !PlayerControl.LocalPlayer.HasModifier<PaintingModifier>()
-               && !PlayerControl.LocalPlayer.HasModifier<SpectatingModifier>()
                && (ChameleonGameModeManager.Instance is { CurrentStage: not TimerStage.Revelation } ||
                    CustomButtonUtilities.IsInPractice());
     }
